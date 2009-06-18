a01aaf5fb8173e19c82ce177eb9813f09ebdd326
Shunichi Yamamoto <yamamo2@users.sourceforge.net>
2009-06-18 17:28:43 +0900

    PICnomeSerial ver0.98 release.

--- src/PicnomeCommunication.java
+++ src/PicnomeCommunication.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with PicnomeSerial. if not, see <http:/www.gnu.org/licenses/>.
  *
- * PicnomeCommunication.java,v.0.97 2009/06/12
+ * PicnomeCommunication.java,v.0.98 2009/06/18
  */
 
 // RXTX
