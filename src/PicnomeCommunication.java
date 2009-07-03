aeba8e8931ee4ee753e4552234ea33c7fb05f158
Shunichi Yamamoto <picnome@tkrworks.net>
2009-07-03 09:47:48 +0900

    Supported new osc command 'output'

--- src/PicnomeCommunication.java
+++ src/PicnomeCommunication.java
@@ -473,6 +473,26 @@ class PicnomeCommunication
     this.oscpin.addListener(this.prefix_tf.getText() + "/pwm", listener);
   }
 
+  public void enableMsgOutput()
+  {
+    OSCListener listener = new OSCListener()
+      {
+        public void acceptMessage(java.util.Date time, OSCMessage message)
+        {
+          Object[] args = message.getArguments();
+
+          try
+          {
+            String str =new String("output " + (Integer)args[0] + " " + (Integer)args[1] + (char)0x0D);
+            //debug debug_tf.setText(str);
+            out.write(str.getBytes());
+          }
+          catch(IOException e){}
+        }
+      };
+    this.oscpin.addListener(this.prefix_tf.getText() + "/output", listener);
+  }
+
   public void enableMsgPrefix()
   {
     OSCListener listener = new OSCListener()
@@ -488,6 +508,7 @@ class PicnomeCommunication
           PicnomeCommunication.this.enableMsgClear();
           PicnomeCommunication.this.enableMsgAdcEnable();
           PicnomeCommunication.this.enableMsgPwm();
+          PicnomeCommunication.this.enableMsgOutput();
         }
       };
     this.oscpin.addListener("/sys/prefix", listener);
@@ -597,6 +618,7 @@ class PicnomeCommunication
     this.enableMsgClear();
     this.enableMsgAdcEnable();
     this.enableMsgPwm();
+    this.enableMsgOutput();
     this.enableMsgPrefix();
     this.enableMsgIntensity();
     this.enableMsgTest();
